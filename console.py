ffd3e7582ab6f8b3c3ca5e5988e46499b6eb1ce7
Denio1212 <shehuden22@yahoo.com>
2023-03-09 20:02:13 +0100

    Console 1

--- console.py
+++ console.py
@@ -4,13 +4,15 @@ main console of this
 """
 import cmd
 import models
-
+from models.base_model import BaseModel
+from shlex import split
 
 class HBNBCommand(cmd.Cmd):
     """
     command interpreter
     """
     prompt = "(hbnb)"
+    classes = ["BaseModel"]
 
     def do_quit(self, line):
         """
@@ -31,6 +33,35 @@ class HBNBCommand(cmd.Cmd):
         """
         :return: an empty line
         """
+
+    def do_create(self, cls):
+        """
+        creates a new instance of BaseModel and then saves it to JSON
+        Prints file Id too.
+        """
+        if not cls:
+            return print("** class name missing **")
+        if ' ' in cls:
+            cls = cls.split(' ')[0]
+        if cls not in HBNBCommand.classes:
+            return print("** class doesn't exist **")
+        else:
+            new = eval(cls)()
+            print(new.id)
+            new.save()
+
+    def do_show(self, args):
+        """
+        Prints the string representation of instance name + Id.
+        """
+        arguments = split(args)
+        if len(arguments) == 0:
+            return print("** class name missing **")
+        if arguments[0] not in HBNBCommand.classes:
+            return print("** class doesn't exist **")
+        if len(arguments) == 1:
+            print("** instance id missing **")
+        
 
 
 if __name__ == '__main__':
