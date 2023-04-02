406c9c12b67d9ba8f0c92f1dc61ee8ac42aedf83
Denio <shehuden22@yahoo.com>
2023-04-02 13:06:37 +0200

    console

--- console.py
+++ console.py
@@ -3,13 +3,8 @@
 import cmd
 from shlex import split as sp
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 import models
+from models.engine.file_storage import FileStorage as storage
 
 
 class HBNBCommand(cmd.Cmd):
