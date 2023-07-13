9fa0a6b807ea5d84684923df86cb20cb72dfcac8
Denio <shehuden22@yahoo.com>
2023-07-13 18:15:05 +0200

    t2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,12 +6,6 @@ from models.base_model import BaseModel
 import os
 import json
 from os import path
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class FileStorage:
