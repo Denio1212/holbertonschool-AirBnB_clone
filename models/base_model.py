3776c58c974173ad37bc5d1a430d55e3bbc05540
Denio <shehuden22@yahoo.com>
2023-07-13 18:06:50 +0200

    t1

--- models/base_model.py
+++ models/base_model.py
@@ -2,53 +2,51 @@
 """
 Houses the base model of the other classes
 """
+
 import models
-import uuid
-from datetime import datetime
+from uuid import uuid4
+from datetime import datetime as dt
 
 
-class BaseModel:
-    """
-    base of goods
-    """
+class BaseModel():
+    """BaseModel class."""
+
     def __init__(self, *args, **kwargs):
-        """
-        constructor
-        :param id: id of object instance
-        :param created_at: where it was created at
-        :param updated_at: where it was updated at
-        """
+        """Initialize BaseModel"""
         if kwargs:
-            for i, j in kwargs.items():
-                if i == "created_at" or i == "updated_at":
-                    setattr(self, i, datetime.strptime(j, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif i == "__class__":
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, dt.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k == "__class__":
                     continue
                 else:
-                    setattr(self, i, j)
+                    setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.id = str(uuid4())
+            self.created_at = dt.utcnow()
+            self.updated_at = dt.utcnow()
             models.storage.new(self)
 
-    # String representation of instance
     def __str__(self):
-        string = "[{}]".format(self.__class__.__name__)
-        string += " ({}) {}".format(self.id, self.__dict__)
-        return string
+        """Prints : [<class name>] (<self.id>) <self.__dict__>"""
+        cls_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
 
-    # Updates datetime and calls storage.save method
     def save(self):
-        self.updated_at = datetime.utcnow()
+        """Updates the public instance attribute updated_at with
+        the current datetime."""
+        self.updated_at = dt.utcnow()
         models.storage.save()
-        return self.updated_at
 
-    # Json serialisation dict representation
     def to_dict(self):
-        new_d = {}
-        new_d = self.__dict__.copy()
-        new_d["__class__"] = str(self.__class__.__name__)
-        new_d['created_at'] = self.created_at.isoformat()
-        new_d['updated_at'] = self.updated_at.isoformat()
-        return new_d
+        """Returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        d = {}
+        for k, v in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                d[k] = dt.isoformat(v)
+            else:
+                d[k] = v
+        d["__class__"] = self.__class__.__name__
+        return d
